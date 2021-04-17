38c1fd9caac40c16e60f007110e045ba61fe10e0
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-04-18 00:25:51 +0800

    set default labels

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,7 +7,11 @@ d3.json(samples).then(function(bdata) {
     var dem_info = bdata.metadata;
     // console.log(dem_info);
     var otus = bdata.samples;
-    console.log(otus[0])
+    
+    // Default data
+    def_subj_ids = subj_ids[0];
+    def_dem_info = dem_info[0];
+    def_otus = otus[0];
 
     subj_id_fill(subj_ids);
     // console.log(dem_info);
@@ -23,31 +27,23 @@ d3.json(samples).then(function(bdata) {
      }
 
     //  Info Panel
-     dem_info_fill(dem_info);
-     function dem_info_fill(dem_info)  {
+     dem_info_fill(def_dem_info);
+     function dem_info_fill(def_dem_info)  {
       default_deminfo = d3.select("#sample-metadata").html(`
-      <p><strong>Id:</strong> ${dem_info[0].id}</p>
-      <p><strong>Ethnicity:</strong> ${dem_info[0].ethnicity}</p>
-      <p><strong>Gender:</strong> ${dem_info[0].gender}</p>
-      <p><strong>Age:</strong> ${dem_info[0].age}</p>
-      <p><strong>Location:</strong> ${dem_info[0].location}</p>
-      <p><strong>BB Type:</strong> ${dem_info[0].bbtype}</p>
-      <p><strong>WFREQ:</strong> ${dem_info[0].wfreq}</p>
+      <p><strong>Id:</strong> ${def_dem_info.id}</p>
+      <p><strong>Ethnicity:</strong> ${def_dem_info.ethnicity}</p>
+      <p><strong>Gender:</strong> ${def_dem_info.gender}</p>
+      <p><strong>Age:</strong> ${def_dem_info.age}</p>
+      <p><strong>Location:</strong> ${def_dem_info.location}</p>
+      <p><strong>BB Type:</strong> ${def_dem_info.bbtype}</p>
+      <p><strong>WFREQ:</strong> ${def_dem_info.wfreq}</p>
       `)}
 
-      // hbchart_fill(otus[0]);
-
-      // var d_hb = otus[0];
-      // d_hbdata = d_hb.sample_values.slice(0, 10);
-      // d_hblabels = d_hb.otu_ids.slice(0, 10);
-      // default_hbchart_labels = otus[0].otu_ids.slice(0, 10);
-      // console.log(d_hbdata);
-      // console.log(d_hblabes)
 
       // Bar Chart
-      var d_hbdata = otus[0].sample_values.slice(0, 10).reverse();
+      var d_hbdata = def_otus.sample_values.slice(0, 10).reverse();
       // console.log(d_hbdata)
-      var d_hbotu_ids = otus[0].otu_ids.slice(0, 10).reverse();
+      var d_hbotu_ids = def_otus.otu_ids.slice(0, 10).reverse();
       // console.log(d_hbotu_ids)
       var d_hblabels = d_hbotu_ids.map(d => "OTU " + d );
       console.log(`${d_hblabels}`)
@@ -70,8 +66,9 @@ d3.json(samples).then(function(bdata) {
 
       Plotly.newPlot("bar", dhbData, layout);
 
-      var gdata = dem_info[0].wfreq;
+      var gdata = def_dem_info.wfreq;
       console.log(gdata)
+
 
       // Gauge Chart
       var data = [
@@ -92,11 +89,9 @@ d3.json(samples).then(function(bdata) {
 
 
       // Bubble Chart
-      var all_otu_ids = otus[0].otu_ids
-      var all_sample_values = otus[0].sample_values
+      var all_otu_ids = def_otus.otu_ids
+      var all_sample_values = def_otus.sample_values
       console.log(all_otu_ids)
-
-
 
       var trace1 = {
         x: all_otu_ids,
@@ -120,50 +115,4 @@ d3.json(samples).then(function(bdata) {
       
       Plotly.newPlot("bubble", data, layout);
 
-      // // part of data to input
-      // var traceGauge = {
-      //   type: 'pie',
-      //   showlegend: false,
-      //   hole: 0.4,
-      //   rotation: 90,
-      //   values: [ 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
-      //   text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
-      //   direction: 'clockwise',
-      //   textinfo: 'text',
-      //   textposition: 'inside',
-      //   marker: {
-      //     colors: ['','','','','','','','','','white'],
-      //     labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
-      //     hoverinfo: 'label'
-      //   }
-      // }
-
-      // // needle
-      // var degrees = 50, radius = .9;
-      // var radians = degrees * Math.PI / 180;
-      // var x = (-1 * radius * Math.cos(radians) * wfreqNum);
-      // var y = radius * Math.sin(radians);
-
-      // var gaugeLayout = {
-      //   shapes: [{
-      //     type: 'line',
-      //     x0: 0.5,
-      //     y0: 0.5,
-      //     x1: 0.6,
-      //     y1: 0.6,
-      //     line: {
-      //       color: 'black',
-      //       width: 3
-      //     }
-      //   }],
-      //   title: 'Chart',
-      //   xaxis: {visible: false, range: [-1, 1]},
-      //   yaxis: {visible: false, range: [-1, 1]}
-      // }
-
-      // var dataGauge = [traceGauge]
-
-      // Plotly.newPlot("gauge", dataGauge, gaugeLayout);
-
   });
-
