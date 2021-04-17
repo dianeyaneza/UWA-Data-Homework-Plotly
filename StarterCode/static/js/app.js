c5926b86b85c0c1244ba1bb99741de40cefcc30d
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-04-18 01:32:35 +0800

    updated prep for change event

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,49 +1,47 @@
 var samples = ("../data/samples.json")
 
 d3.json(samples).then(function(bdata) {
-    // console.log(bdata)
-    var subj_ids = bdata.names;
-    // console.log(subj_ids);
-    var dem_info = bdata.metadata;
-    // console.log(dem_info);
-    var otus = bdata.samples;
+  // console.log(bdata)
+  var subj_ids = bdata.names;
+  // console.log(subj_ids);
+  var dem_info = bdata.metadata;
+  // console.log(dem_info);
+  var otus = bdata.samples;
+  console.log(otus[0])
     
-    // Default data
-    def_subj_ids = subj_ids[0];
-    def_dem_info = dem_info[0];
-    def_otus = otus[0];
 
-    subj_id_fill(subj_ids);
-    // console.log(dem_info);
-    // console.log(otus);
-    function subj_id_fill(subj_ids) {
-      var chosenid = d3.select("#selDataset");
-      chosenid.html("");
-      for (var i = 0; i < subj_ids.length; i++) {
-        var optionrow = chosenid.append("option");
-            optionrow.text(subj_ids[i]);
-            optionrow.attr("value", subj_ids[i]);
-     };
-     }
+  subj_id_fill(subj_ids);
+  // console.log(dem_info);
+  // console.log(otus);
+  function subj_id_fill(subj_ids) {
+    var chosenid = d3.select("#selDataset");
+    chosenid.html("");
+    for (var i = 0; i < subj_ids.length; i++) {
+      var optionrow = chosenid.append("option");
+          optionrow.text(subj_ids[i]);
+          optionrow.attr("value", subj_ids[i]);
+   };
+   }
+
 
     //  Info Panel
-     dem_info_fill(def_dem_info);
-     function dem_info_fill(def_dem_info)  {
-      default_deminfo = d3.select("#sample-metadata").html(`
-      <p><strong>Id:</strong> ${def_dem_info.id}</p>
-      <p><strong>Ethnicity:</strong> ${def_dem_info.ethnicity}</p>
-      <p><strong>Gender:</strong> ${def_dem_info.gender}</p>
-      <p><strong>Age:</strong> ${def_dem_info.age}</p>
-      <p><strong>Location:</strong> ${def_dem_info.location}</p>
-      <p><strong>BB Type:</strong> ${def_dem_info.bbtype}</p>
-      <p><strong>WFREQ:</strong> ${def_dem_info.wfreq}</p>
-      `)}
+    dem_info_fill(dem_info);
+    function dem_info_fill(dem_info)  {
+     default_deminfo = d3.select("#sample-metadata").html(`
+     <p><strong>Id:</strong> ${dem_info[0].id}</p>
+     <p><strong>Ethnicity:</strong> ${dem_info[0].ethnicity}</p>
+     <p><strong>Gender:</strong> ${dem_info[0].gender}</p>
+     <p><strong>Age:</strong> ${dem_info[0].age}</p>
+     <p><strong>Location:</strong> ${dem_info[0].location}</p>
+     <p><strong>BB Type:</strong> ${dem_info[0].bbtype}</p>
+     <p><strong>WFREQ:</strong> ${dem_info[0].wfreq}</p>
+     `)}
 
 
       // Bar Chart
-      var d_hbdata = def_otus.sample_values.slice(0, 10).reverse();
+      var d_hbdata = otus[0].sample_values.slice(0, 10).reverse();
       // console.log(d_hbdata)
-      var d_hbotu_ids = def_otus.otu_ids.slice(0, 10).reverse();
+      var d_hbotu_ids = otus[0].otu_ids.slice(0, 10).reverse();
       // console.log(d_hbotu_ids)
       var d_hblabels = d_hbotu_ids.map(d => "OTU " + d );
       console.log(`${d_hblabels}`)
@@ -66,7 +64,7 @@ d3.json(samples).then(function(bdata) {
 
       Plotly.newPlot("bar", dhbData, layout);
 
-      var gdata = def_dem_info.wfreq;
+      var gdata = dem_info[0].wfreq;
       console.log(gdata)
 
 
@@ -89,8 +87,8 @@ d3.json(samples).then(function(bdata) {
 
 
       // Bubble Chart
-      var all_otu_ids = def_otus.otu_ids
-      var all_sample_values = def_otus.sample_values
+      var all_otu_ids = otus[0].otu_ids
+      var all_sample_values = otus[0].sample_values
       console.log(all_otu_ids)
 
       var trace1 = {
