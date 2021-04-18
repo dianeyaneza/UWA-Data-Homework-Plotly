8a8721467cf246c4852e5cc74e98f0bb1f7351f1
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-04-18 19:35:11 +0800

    attempted change event listener

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,12 +7,11 @@ d3.json(samples).then(function(bdata) {
   var dem_info = bdata.metadata;
   // console.log(dem_info);
   var otus = bdata.samples;
-  console.log(otus[0])
-    
+  // console.log(otus)
 
+
+  // Dropdown menu to select ID
   subj_id_fill(subj_ids);
-  // console.log(dem_info);
-  // console.log(otus);
   function subj_id_fill(subj_ids) {
     var chosenid = d3.select("#selDataset");
     chosenid.html("");
@@ -23,8 +22,29 @@ d3.json(samples).then(function(bdata) {
    };
    }
 
+  // //  Event listener when new ID is selected not working yet
+  // id_selection = d3.select("#selDataset").on("change", optionChanged);
 
-    //  Info Panel
+  // // ON CHANGE: 'optionChanged' from dropdown selection
+  //   function optionChanged(id_no){
+  //     dem_info_fill(id_no);
+  //     charts_data(id_no);
+  //   };
+
+    // // New Info Panel try 
+    // function deminfo_fill(id_no){
+    //   d3.json(samples).then((bdata)=>{
+    //     var dem_info = bdata.metadata;
+    //     var result = dem_info.filter(dem => dem.id_no.toString() === id_no)[0];
+    //     var dem_panel = d3.select("#sample-metadata");
+    //     dem_panel.html("");
+    //     Object.defineProperties(result).forEach((key)=> {
+    //       dem_panel.append("h4").text(key[0].toUpperCase()+ ": " + key[1] + "\n");
+    //     });
+    //   });
+    // }
+
+    //  Info Panel good
     dem_info_fill(dem_info);
     function dem_info_fill(dem_info)  {
      default_deminfo = d3.select("#sample-metadata").html(`
@@ -37,12 +57,11 @@ d3.json(samples).then(function(bdata) {
      <p><strong>WFREQ:</strong> ${dem_info[0].wfreq}</p>
      `)}
 
-
       // Bar Chart
       var d_hbdata = otus[0].sample_values.slice(0, 10).reverse();
       // console.log(d_hbdata)
       var d_hbotu_ids = otus[0].otu_ids.slice(0, 10).reverse();
-      // console.log(d_hbotu_ids)
+      console.log(d_hbotu_ids)
       var d_hblabels = d_hbotu_ids.map(d => "OTU " + d );
       console.log(`${d_hblabels}`)
 
@@ -64,15 +83,16 @@ d3.json(samples).then(function(bdata) {
 
       Plotly.newPlot("bar", dhbData, blayout);
 
+
+      // Gauge Chart
+
       var gdata = dem_info[0].wfreq;
       console.log(gdata)
 
-
-      // Gauge Chart
       var data = [
         {
           domain: { x: [0, 1], y: [0, 1] },
-          value: 2,
+          value: gdata,
           title: { text: "<b>Belly Button Washing Frequency</b><br>Washes per Week"},
           type: "indicator",
           bar: { color: "blue" },
@@ -97,9 +117,9 @@ d3.json(samples).then(function(bdata) {
         text: all_otu_ids,
         mode: 'markers',
         marker: {
-          color: ['rgb(191,233,115)', 'rgb(213,198,183)', 'rgb(44, 160, 101)', 'rgb(174,143,113)',  'rgb(213,198,183)', 'rgb(77,83,168)'],
-          opacity: [1, 0.8, 0.6, 0.4, 0.2],
-          size: all_sample_values
+          size: all_sample_values,
+          color: all_sample_values,
+          colorscale: "Jet"
         }
       };
       
